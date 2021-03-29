08dc6358db05bd02fe164f8763cd2415a338da87
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2021-03-29 22:07:24 +0300

    refactor: do not use tag selectors (unless deliberately resetting the default styles) according to rule 3.4 in html-and-css

--- app.js
+++ app.js
@@ -33,20 +33,23 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="input-checkbox"
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task input-task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="edit btn";
 
-    deleteButton.className="delete";
+    deleteButton.className="delete btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "btn-image"
     deleteButton.appendChild(deleteButtonImg);
-
+    
+    listItem.className = "list";
 
     //and appending.
     listItem.appendChild(checkBox);
